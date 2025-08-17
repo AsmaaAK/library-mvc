75e2aea9a57d22ecf420db8a0acfe0eba1432dae
AsmaaAK <asoomak97@gmail.com>
2025-08-17 09:51:44 -0700

    add autoload

---
+++ App/Public/index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+
+use App\Core\Router;
+
+$router = new Router();
+require __DIR__.'/../routes/web.php';
+$router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+spl_autoload_register(function($class){
+   
+    $prefix = 'App\\';
+    if(strncmp($class, $prefix, strlen($prefix)) !== 0){
+        return;
+    }
+    $relativeClass = str_replace('\\','/', substr($class, strlen($prefix)));
+  
+    $file = __DIR__ . '/app/' . $relativeClass . '.php'; 
+    if(file_exists($file)){
+        require $file;
+    }
+});
